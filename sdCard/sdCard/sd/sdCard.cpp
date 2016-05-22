6461dd43503c6a9d7ba142029f5654594593afc6
Tonni Follmann <201504573@post.au.dk>
2016-05-22 19:14:50 +0200

    added a sendCommand function to the sd card handler, still need a bunch of work, but since i can't test currently it is a bit of a guess work.

--- sdCard/sdCard/sd/sdCard.cpp
+++ sdCard/sdCard/sd/sdCard.cpp
@@ -9,3 +9,27 @@
 // REV. DATE/AUTHOR CHANGE DESCRIPTION
 // 1.0 22-05-2016/Tonni Nybo Follmann created the initial header file.
 //========================================================================
+
+
+//=============================================================
+// METHOD : sendCommand
+// DESCR. : takes a cmd index, an argument and a CRC and creates
+// and sends the command to the sd card.
+//=============================================================
+unsigned char sdCard::sendCommand( unsigned char cmdindex, unsigned long argument, unsigned char CRC )
+{
+	unsigned char *argument_byte_pointer = (unsigned char*)&argument; // creating a pointer to extract bytes to be send.
+	spi_obj.writeByte(0b01000000 | cmdindex);
+	spi_obj.writeByte(argument_byte_pointer[0]);
+	spi_obj.writeByte(argument_byte_pointer[1]);
+	spi_obj.writeByte(argument_byte_pointer[3]);
+	spi_obj.writeByte(argument_byte_pointer[4]);
+	spi_obj.writeByte(CRC);
+	unsigned char response;
+	do 
+	{
+		response = spi_obj.recieveByte();
+	} while (response = 0xFF);
+	return response;
+}
+
--- sdCard/sdCard/sd/sdCard.h
+++ sdCard/sdCard/sd/sdCard.h
@@ -9,12 +9,39 @@
 // REV. DATE/AUTHOR CHANGE DESCRIPTION
 // 1.0 22-05-2016/Tonni Nybo Follmann created the initial header file.
 //========================================================================
-
+#include <avr/io.h>
+#include "../SPI/SPI.h"
 
 #ifndef SDCARD_H_
 #define SDCARD_H_
 
+#define SS 0 // using Port B pin 0 as SS ; pin 53 on arduino board.
 
+// helper type to make it easier to handle commands.
+struct SD_Card_command{
+	union{
+		unsigned char command;
+		unsigned int 
+		};
+	};
+
+//=====================================
+// CLASS : sdCard
+// DESCR. : represends the SD card controlled using SPI in master mode
+// on arduino mega2560, the sd card i used in raw block access mode.
+//=====================================
+
+class sdCard{
+	public:
+		sdCard(int speed);
+		bool init(); // initializes the SD card.
+		bool writeBlock(unsigned char adress, const unsigned char data[], int data_length); // data_length must be less than 512.
+		bool readBlock(unsigned char adress, unsigned char data[], int data_length); // will fill the supplied array with the content of the block on the sd card.
+	private:
+		SPI spi_obj;
+		int speed;
+		unsigned char sendCommand(unsigned char cmdindex, unsigned long argument, unsigned char CRC); // used internally to send a sd command.
+};
 
 
 
