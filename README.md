a6f2f8c41bac335a444ab6b634eb8d5f2d7d078f
ilithanos <ilithanos@users.noreply.github.com>
2016-05-22 11:06:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SD-Card-Software
+repo for the sd card driver software for raw block access through SPI with atmega2560
---
+++ sdCard/sdCard/sdCard.cpp
@@ -0,0 +1,17 @@
+/*
+ * sdCard.cpp
+ *
+ * Created: 22-05-2016 11:08:25
+ *  Author: Tonni
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
